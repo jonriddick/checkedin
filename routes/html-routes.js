34906e24199578344ad4e924b766b2eae04d3c44
stevemurphy256 <smurphy5884@icloud.com>
2017-10-15 12:39:38 -0400

    html routes update wasn't included at end of class push

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -28,16 +28,20 @@ module.exports = function(app) {
 	});
 
 
+	app.get("/event", function(req, res) {
+	  res.render("event", { });
+	});
+
 	// app.post("/checkin", function(req, res) {
 	//   res.render("index", { });
 	// });
 
-	app.get("/event/:eventName", function(req, res) {
-		db.Event.get(where: { event_name_sanitized: req.params.eventName }).then(function(event){
-	         res.render("event", { event: event });		
-		})
+	// app.get("/event/:eventName", function(req, res) {
+	// 	db.Event.get(where: { event_name_sanitized: req.params.eventName }).then(function(event){
+	//          res.render("event", { event: event });		
+	// 	})
 	  
-	});
+	// });
 
 	// app.get("/api/*", function(req, res) {
 	//     res.render("index", { });
