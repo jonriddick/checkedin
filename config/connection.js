33783e978dba5e11a561d22ad5d73ddffdc3da5b
stevemurphy256 <smurphy5884@icloud.com>
2017-10-16 14:48:27 -0400

    heroku?

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Sundrop7@",
+    database: "checkedin_db"
+  });
+};
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
