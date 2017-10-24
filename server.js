8d55fd41559cc0b2ab15e8007d48abfa6e55cc1f
stevemurphy256 <smurphy5884@icloud.com>
2017-10-23 21:18:28 -0400

    heroku 3

--- server.js
+++ server.js
@@ -80,24 +80,24 @@ var mysql = require("mysql");
 var connection;
 
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "checkedin_db"
-  });
-};
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     database: "checkedin_db"
+//   });
+// };
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 
 
